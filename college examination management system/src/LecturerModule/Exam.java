5afe72c905f2232d1f8aa43c98712ae482fba104
Ahmed0Nagah <168332836+Ahmed0Nagah@users.noreply.github.com>
2024-12-01 00:19:58 +0200

    modifying the files

---
+++ college examination management system/src/LecturerModule/Exam.java
@@ -0,0 +1,45 @@
+package LecturerModule;
+
+import java.util.ArrayList;
+
+public class Exam {
+    private ArrayList<String> questions;
+    private String duration;
+    private ArrayList<String> correctAnswers;
+
+    public Exam(ArrayList<String>questions , String duration,
+            ArrayList<String> correctAnswers) {
+        
+        this.questions = questions;
+        this.duration = duration;
+        this.correctAnswers = correctAnswers;
+    }
+
+    public void setCorrectAnswers(ArrayList<String> correctAnswers) {
+        this.correctAnswers = correctAnswers;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public void setQuestions(ArrayList<String> questions) {
+        this.questions = questions;
+    }
+
+    public ArrayList<String> getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public ArrayList<String> getQuestions() {
+        return questions;
+    }
+    
+    
+    
+    
+}
--- college examination management system/src/UserModule/User.java
+++ college examination management system/src/UserModule/User.java
@@ -1,17 +1,17 @@
 package UserModule;
 
-public class User {
-    private final int id;
+public abstract class User {
     private String userName;
     private String password;
 
-    public User(int id,String userName, String password) {
-        this.id = id;
-        this.password = password;
+    public User(String userName, String password) {
         this.userName = userName;
+        this.password = password;
     }
+    protected abstract void setID(int id);
+    public abstract int getID();
 
-    public void setPassword(String password) {
+    protected void setPassword(String password) {
         this.password = password;
     }
 
@@ -19,20 +19,13 @@ public class User {
         this.userName = userName;
     }
 
-    public int getId() {
-        return id;
-    }
-
+    
     public String getPassword() {
         return password;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getName(User u){
+        return u.userName;
     }
-    
-    
-    
-    
     
 }
