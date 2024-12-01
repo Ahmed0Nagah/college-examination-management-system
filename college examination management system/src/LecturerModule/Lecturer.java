57a10e1b33b20bc26f497e3060006c4e0a8a2ec1
Ahmed0Nagah <168332836+Ahmed0Nagah@users.noreply.github.com>
2024-12-01 22:15:15 +0200

    update

--- college examination management system/src/LecturerModule/Lecturer.java
+++ college examination management system/src/LecturerModule/Lecturer.java
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 package LecturerModule;
 
 
 public class Lecturer extends UserModule.User{
         private static int id;
 
-    Lecturer(String name, String password) {
+    public Lecturer(String name, String password) {
         super(name, password);
         id++;
     }
@@ -17,18 +16,7 @@ public class Lecturer extends UserModule.User{
     public int getID() {
         return id;
     }    
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package LecturerModule;
-
-/**
- *
- * @author Ahmed Nagah
- */
-public class Lecturer {
     
->>>>>>> parent of b9a1be2 (jiuhh)
+    
 }
+
--- college examination management system/src/UserModule/User.java
+++ college examination management system/src/UserModule/User.java
@@ -1,6 +1,12 @@
 package UserModule;
 
+import LecturerModule.Lecturer;
+import java.io.*;
+import java.util.Scanner;
+
 public abstract class User {
+    
+    
     private String userName;
     private String password;
 
@@ -28,4 +34,40 @@ public abstract class User {
         return u.userName;
     }
     
+    public  void printAllContnet(String pathName){
+        try(Scanner fileReader = new Scanner(new File(pathName))) {
+            while(fileReader.hasNext()){
+                String line = fileReader.nextLine();
+                String [] arr = line.split(",");
+                
+                if(arr.length <= 4){
+                    String id = arr[0].trim();
+                    String username = arr[1].trim();
+                    String passwordd = arr[2].trim();
+                    String courseName = arr[3].trim();
+                    
+                    System.out.println("The id is : "+ id);
+                    System.out.println("The username is : "+ username);
+                    System.out.println("The password is : "+ passwordd);
+                    System.out.println("The course name is : "+ courseName);
+                    System.out.println("************************");
+                }
+                else{
+                    System.out.println("Invalid format!\n");
+                }
+            }
+            
+            
+        } catch (Exception e) {
+            System.out.println("The file not found!\n");
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        User user = new Lecturer("Ahmed", "1234");
+        user.printAllContnet("src\\Lecturers.txt");
+        
+        
+    }
 }
