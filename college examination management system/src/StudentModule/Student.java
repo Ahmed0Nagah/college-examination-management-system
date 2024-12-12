ecb09612a5fc8b1035924acf94ce32025492b39e
DohaShafey <doha.hamed.2006@gmail.com>
2024-12-12 22:23:59 +0200

    Update Student.java

--- college examination management system/src/StudentModule/Student.java
+++ college examination management system/src/StudentModule/Student.java
@@ -1,14 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package StudentModule;
 
-/**
- *
- * @author Ahmed Nagah
- */
-public class Student {
-    int std_age = 25;
+public class Student extends UserModule.User{
+    private static int id = 100;
     
+    
+    public Student(String name , String password){
+        super(name, password);
+        id++;
+    }
+
+    
+    @Override
+    public int getID(){
+        return id;
+    }
 }
